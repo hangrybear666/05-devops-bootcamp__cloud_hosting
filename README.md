4db40eaa8bd739bd765192dbee088be6580cac61
hangrybear666 <corey.lauster@gmail.com>
2024-07-15 00:10:21 +0200

    Added remote NVM / NPM install script

---
+++ README.md
@@ -0,0 +1,13 @@
+# Basic Cloud Hosting in Linux VM on DigitalOcean
+
+This repo contains simple Bash Script files to establish an ssh connection to a cloud hosted linux virtual machine. After having established connection, some programs such as node are installed, a deployment package is copied via scp and then run on the remote machine.
+
+## Setup
+
+Pull the repository locally by running 
+```
+  git clone 
+```
+
+## Usage
+
---
+++ remote-install-node-npm.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+REMOTE_ADDRESS=164.92.162.21
+INITIAL_USER="root"
+NEW_USER="node-runner"
+PUBLIC_KEY=$(cat $HOME/.ssh/id_rsa.pub)
+
+# ask for user input to avoid password exposure in git
+read -p "Please provide password for new user $NEW_USER: " NEW_USER_PW
+
+ssh $INITIAL_USER@$REMOTE_ADDRESS <<EOF
+# reset prior user and the respective home folder
+userdel -r $NEW_USER
+
+#create new user
+useradd -m $NEW_USER
+echo "$NEW_USER:$NEW_USER_PW" | chpasswd
+
+# switch to new user
+su - $NEW_USER
+
+# add public key to new user's authorized keys
+mkdir .ssh
+cd .ssh
+touch authorized_keys
+echo "created .ssh/authorized keys file"
+echo "$PUBLIC_KEY" > authorized_keys
+echo "added public key to authorized_keys file of new user."
+EOF
+
+# ssh into remote with newly created user to download Node Version Manager (NVM)
+ssh $NEW_USER@$REMOTE_ADDRESS <<EOF
+# download nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+EOF
+
+# restart ssh shell so newly added nvm command can be found
+ssh $NEW_USER@$REMOTE_ADDRESS <<EOF
+#install node and npm
+nvm install 22
+EOF
